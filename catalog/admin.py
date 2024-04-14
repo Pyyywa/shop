c6fe770406689bd6285c578e57713c8ae1629a13
Rusha <pyyywa@gmail.com>
2024-04-14 11:46:00 +0300

    edited hw3

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,12 +6,11 @@ from catalog.models import Product, Category
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'product_name', 'quantity_per_unit', 'category')
-    search_fields = ('product_name',  'desc')
+    search_fields = ('product_name', 'desc')
     list_filter = ('category',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    #list_display = ('id', 'category_name', 'description')
+    # list_display = ('id', 'category_name', 'description')
     list_display = ('id', 'category_name')
-
--- catalog/models.py
+++ catalog/models.py
@@ -1,20 +1,22 @@
-import django.utils.timezone
 from django.db import models
+
 
 NULLABLE = {'blank': True, 'null': True}
 
+
 class Product(models.Model):
-    product_name = models.CharField(max_length=50, verbose_name = 'наименование')
-    desc = models.TextField(verbose_name = 'описание', **NULLABLE)
-    image = models.ImageField(upload_to='products/', verbose_name = 'превью', **NULLABLE)
-    category = models.CharField(max_length=50, verbose_name = 'категория')
-    quantity_per_unit = models.PositiveIntegerField(verbose_name = 'цена за шт.', default='0')
-    сreation_date = models.DateField(verbose_name = 'дата создания')
-    last_change_date = models.DateTimeField(verbose_name = 'дата последнего изменения', **NULLABLE)
+    product_name = models.CharField(max_length=50, verbose_name ='наименование')
+    desc = models.TextField(verbose_name ='описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name ='превью', **NULLABLE)
+    category = models.CharField(max_length=50, verbose_name ='категория')
+    quantity_per_unit = models.PositiveIntegerField(verbose_name ='цена за шт.', default='0')
+    сreation_date = models.DateField(verbose_name ='дата создания')
+    last_change_date = models.DateTimeField(verbose_name ='дата последнего изменения', **NULLABLE)
 
 
     def __str__(self):
         return f'{self.category} {self.product_name} '
+
 
     class Meta():
         verbose_name = 'продукт'
@@ -22,8 +24,8 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=50, verbose_name = 'наименование')
-    description = models.TextField(verbose_name = 'описание', **NULLABLE)
+    category_name = models.CharField(max_length=50, verbose_name ='наименование')
+    description = models.TextField(verbose_name ='описание', **NULLABLE)
 
     def __str__(self):
         return f'{self.id}. {self.category_name}'
